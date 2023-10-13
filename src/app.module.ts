e0940bb602528448dc1fab4257d580b95b7d705a
Pablo Solano <pablosolano61098@gmail.com>
2023-10-13 08:39:04 -0500

    Preparar mongodb y envs

---
+++ src/app.module.ts
@@ -0,0 +1,38 @@
+import { join } from 'path'; // en Node
+import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { PokemonModule } from './pokemon/pokemon.module';
+import { CommonModule } from './common/common.module';
+import { SeedModule } from './seed/seed.module';
+import { ConfigModule } from '@nestjs/config';
+import { EnvConfiguration } from './config/env.config';
+import { JoiValidationSchema } from './config/joi.validation';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      load: [EnvConfiguration],
+      validationSchema: JoiValidationSchema,
+    }), //To read .env variables
+
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+
+    MongooseModule.forRoot(process.env.MONGODB, { dbName: 'pokemonsdb' }),
+
+    PokemonModule,
+
+    CommonModule,
+
+    SeedModule,
+  ],
+})
+export class AppModule {
+  constructor() {
+    console.log(process.env.DEFAULT_LIMIT);
+    console.log(process.env.defaultLimit);
+  }
+}
